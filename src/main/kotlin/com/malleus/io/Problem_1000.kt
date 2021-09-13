66939328b00a7a8551945d2e7a74b30f82a25072
Junghun Yang <junghun.yang.0914@linecorp.com>
2021-09-14 04:37:28 +0900

    kotlin io 2

--- src/main/kotlin/com/malleus/io/Problem_1000.kt
+++ src/main/kotlin/com/malleus/io/Problem_1000.kt
@@ -1,7 +1,14 @@
 package com.malleus.io
 
-import java.util.Scanner
+import java.io.*
 
-fun main(args: Array<String>) = with(Scanner(System.`in`)) {
-    println(nextInt() + nextInt())
+fun main(args: Array<String>) {
+    BufferedReader(InputStreamReader(System.`in`))
+    println(readLine()!!.split(" ").sumOf { it.toInt() })
 }
+
+//import java.util.Scanner
+
+//fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+//    println(nextInt() + nextInt())
+//}
